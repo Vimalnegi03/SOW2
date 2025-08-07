620d3ea1a60cff501fa02b0dc7ad00bec652faf8
Vimal <vm.21cs48@thdcihet.ac.in>
2025-08-07 13:01:48 +0530

    done with a font change

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
   allowFullScreen
 ></iframe>
     </div>
-    <p className="font-serif text-3xl  text-justify w-full lg:w-1/2 ml-4 ">
+    <p className="font-serif text-2xl  text-justify w-full lg:w-1/2 ml-4 ">
      <span className='text-orange-400'> Lend A Hand India,</span> a Pune based NGO working in the field of vocational education in secondary 
       schools for last 10 years in 10 states across India in partnership with Governments of <span className='text-orange-400'>Maharashtra, 
       Gujarat, Andhra Pradesh, Telangana, NCT of Delhi, Odisha, Madhya Pradesh, Haryana, UT Of Diu and Daman. </span> 
